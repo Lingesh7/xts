86547cea293860c44f6319f6cee90af5ae5f1e9c
Lingesh7 <41271032+Lingesh7@users.noreply.github.com>
2020-12-20 13:25:09 +0530

    modified xts with setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xts',
+      version='0.1',
+      description='xts with setup.py',
+      url='https://github.com/symphonyfintech/xts-pythonclient-api-sdk',
+      author='someone',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['XTConnect'],
+      zip_safe=False)
